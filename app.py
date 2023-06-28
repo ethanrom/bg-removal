a1af93c11dc91275a2c295dcd8a9c547c20811f2
ethanrom <116474403+ethanrom@users.noreply.github.com>
2023-06-28 19:36:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ from PIL import Image
 import numpy as np
 import cv2
 
+background_removed_image = None
+
 def perform_perspective_correction(image):
     img = np.array(image)
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -30,6 +32,8 @@ def perform_perspective_correction(image):
     return corrected_image
     
 def main():
+    global background_removed_image
+
     st.title("Image Background Remover")
 
     uploaded_image = st.file_uploader("Upload an image", type=["png", "jpg", "jpeg"])
@@ -40,12 +44,12 @@ def main():
     if st.button("Remove Background"):
         with st.spinner("Removing background..."):
             if uploaded_image is not None:
-                output_image = remove(image)
-                st.image(output_image, caption="Background Removed", use_column_width=True)
+                background_removed_image = remove(image)
+                st.image(background_removed_image, caption="Background Removed", use_column_width=True)
 
                 if st.button("Perspective Correction"):
                     with st.spinner("Performing perspective correction..."):
-                        corrected_image = perform_perspective_correction(output_image)
+                        corrected_image = perform_perspective_correction(background_removed_image)
                         st.image(corrected_image, caption="Perspective Corrected", use_column_width=True)
             else:
                 st.warning("Please upload an image first.")
