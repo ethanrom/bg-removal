ae291b253b62c0382984db5d61c3d709b21f36cb
ethanrom <116474403+ethanrom@users.noreply.github.com>
2023-06-28 19:18:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,31 @@ import streamlit as st
 from rembg import remove
 from PIL import Image
 
+def perform_perspective_correction(image):
+    img = np.array(image)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    _, thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
+
+    contours, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    biggest_contour = max(contours, key=cv2.contourArea)
+    rect = cv2.minAreaRect(biggest_contour)
+    box = cv2.boxPoints(rect)
+    box = np.int0(box)
+
+    width = int(rect[1][0])
+    height = int(rect[1][1])
+
+    src_pts = box.astype("float32")
+    dst_pts = np.array([[0, height - 1],
+                        [0, 0],
+                        [width - 1, 0],
+                        [width - 1, height - 1]], dtype="float32")
+
+    perspective_matrix = cv2.getPerspectiveTransform(src_pts, dst_pts)
+    corrected_image = cv2.warpPerspective(img, perspective_matrix, (width, height))
+
+    return corrected_image
+    
 def main():
     st.title("Image Background Remover")
 
@@ -14,8 +39,12 @@ def main():
         with st.spinner("Removing background..."):
             if uploaded_image is not None:
                 output_image = remove(image)
-
                 st.image(output_image, caption="Background Removed", use_column_width=True)
+
+                if st.button("Perspective Correction"):
+                    with st.spinner("Performing perspective correction..."):
+                        corrected_image = perform_perspective_correction(output_image)
+                        st.image(corrected_image, caption="Perspective Corrected", use_column_width=True)
             else:
                 st.warning("Please upload an image first.")
 
